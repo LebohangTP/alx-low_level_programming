b9a5f5af84d0dce6bac36cd4b843ca3913fd2d86
LebohangTP <tphetoane@yahoo.co.za>
2023-08-11 16:53:46 -0700

    Inventing is a combination of brains and materials

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int d, p;
+
+	for (d = '0' ; d < '9' ; d++)
+
+	for (p = d + 1; p <= '9' ; p++)
+	putchar('\n');
+	return (0);
+}
